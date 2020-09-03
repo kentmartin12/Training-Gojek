ca2b2a1669d6b5642d20abe6c19ee20dcc6790f2
kentmartin12 <kentmartin12@gmail.com>
2020-09-03 21:53:20 +0700

    Second Commit

--- app/src/main/java/com/example/bnccapplication/LookupActivity.kt
+++ app/src/main/java/com/example/bnccapplication/LookupActivity.kt
@@ -2,11 +2,10 @@ package com.example.bnccapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+class LookupActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_lookup)
     }
 }
