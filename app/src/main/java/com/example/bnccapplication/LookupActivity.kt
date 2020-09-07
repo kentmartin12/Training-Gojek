9009d3473495ce718b2ad5470610380eee273502
kentmartin12 <kentmartin12@gmail.com>
2020-09-07 16:30:40 +0700

    Lookup activity
    berhasil pindah activity dari main ke lookup dan sebaliknya

--- app/src/main/java/com/example/bnccapplication/LookupActivity.kt
+++ app/src/main/java/com/example/bnccapplication/LookupActivity.kt
@@ -1,11 +1,26 @@
 package com.example.bnccapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_lookup.*
 
 class LookupActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lookup)
+
+        btnBack.setOnClickListener{
+            openMainActivity()
+        }
+
     }
+
+    private fun openMainActivity(){
+        val intent = Intent(this, MainActivity::class.java).apply {
+            putExtra(MainActivity.EXTRAS, "this is from lookup activity")
+        }
+        startActivity(intent)
+    }
+
 }
--- app/src/main/java/com/example/bnccapplication/MainActivity.kt
+++ app/src/main/java/com/example/bnccapplication/MainActivity.kt
@@ -1,12 +1,30 @@
 package com.example.bnccapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    companion object{
+        const val EXTRAS = "Extras"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnLookup.setOnClickListener{
+            openLookupActivity()
+        }
+    }
+
+    private fun openLookupActivity(){
+        val intent = Intent(this, LookupActivity::class.java).apply {
+            putExtra(EXTRAS, "this is from main activity")
+        }
+        startActivity(intent)
     }
 }
